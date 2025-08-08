ecdcc7c2f243018569035cd1ec0b14250d58d306
Neil-Lin <neillin1023@gmail.com>
2025-08-09 02:12:32 +0800

    fix: config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,10 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
+  ssr: true,
   nitro: {
     prerender: {
-      routes: ["/", "/products", ...dynamicRoutes],
+      routes: ["/", "/products", ...dynamicRoutes, "projects", "blog"],
     },
   },
   modules: [
