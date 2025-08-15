96b2990a208e32bb9d7f9163aaf5311f82257403
Neil-Lin <neillin1023@gmail.com>
2025-08-16 00:02:27 +0800

    wip: test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,12 +13,12 @@ export default defineNuxtConfig({
   },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
-  ssr: true,
-  nitro: {
-    prerender: {
-      routes: ["/", "/products", ...dynamicRoutes, "projects", "blog"],
-    },
-  },
+  // ssr: true,
+  // nitro: {
+  //   prerender: {
+  //     routes: ["/", "/products", ...dynamicRoutes, "projects", "blog"],
+  //   },
+  // },
   modules: [
     "@nuxt/eslint",
     "nuxt-gtag",
