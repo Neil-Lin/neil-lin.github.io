29957bb6ac9369b28deba98de9125634b1426a2d
neillin <neillin@blueplanet.com.tw>
2025-08-22 10:35:27 +0800

    redirect netlify

--- server/middleware/host-redirect.ts
+++
@@ -1,6 +0,0 @@
-export default defineEventHandler((event) => {
-  const host = getRequestHeader(event, "host") || "";
-  if (host.endsWith("netlify.app")) {
-    return sendRedirect(event, `https://neillin.com${event.path}`, 301);
-  }
-});
