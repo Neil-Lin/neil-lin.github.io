bc11d7480b74f7d70008316063d62d7b5fa0fc3b
Neil-Lin <neillin1023@gmail.com>
2025-12-30 01:00:43 +0800

    finish add accesserty pulse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
           src: "https://accesserty.com/pulse.js",
           defer: true,
           // 這是您的專屬追蹤 ID
-          "data-client-id": "Partner_MJRB6G7B_848",
+          "data-client-id": "Partner_MJRBV4ZI_850",
         },
       ],
     },
