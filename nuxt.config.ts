0b1690a17535f260e193dd9560fe00a80a907c47
neillin <neillin@blueplanet.com.tw>
2025-12-29 18:22:45 +0800

    fix: accesserty script id

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,7 +16,7 @@ export default defineNuxtConfig({
           src: "https://accesserty.com/pulse.js",
           defer: true,
           // 這是您的專屬追蹤 ID
-          "data-client-id": "Trial_MJR02VEC_395",
+          "data-client-id": "Partner_MJR0EBVF_126",
         },
       ],
     },
